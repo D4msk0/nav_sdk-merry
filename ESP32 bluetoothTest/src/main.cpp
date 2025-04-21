506ed149107f0c41b6693bc20f6503e096497b68
Merry Damsko <m.d4msk0@gmail.com>
2025-04-21 04:55:26 +0200

    added esp32 part to project

---
+++ ESP32 bluetoothTest/src/main.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+#include "BluetoothSerial.h"
+#include "esp_bt_device.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(115200);
+
+  SerialBT.begin("ESP32test"); // Start eerst Bluetooth!
+  Serial.println("Bluetooth started.");
+
+  // Haal MAC-adres op
+  const uint8_t* mac = esp_bt_dev_get_address();
+  char macStr[18];
+  snprintf(macStr, sizeof(macStr), "%02X:%02X:%02X:%02X:%02X:%02X",
+           mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+  Serial.print("ESP32 Bluetooth MAC Address: ");
+  Serial.println(macStr);
+}
+
+void loop() {
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available()) {
+    Serial.write(SerialBT.read());
+  }
+  delay(20);
+}
